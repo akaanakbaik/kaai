562c6775aaad3582ac852ab7cf4f137240b1d4ca
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2025-12-22 20:51:12 +0700

    Configure Axios defaults in main.jsx
    Set default Axios base URL and content type header.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://kaai-api.akadev.me";
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
