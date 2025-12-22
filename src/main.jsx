69f94f7250285b7b9e00371f2167b06c52db5f13
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2025-12-22 23:06:42 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import axios from 'axios';
+import { inject } from '@vercel/analytics';
+import { HelmetProvider } from 'react-helmet-async';
+
+inject();
 
 axios.defaults.baseURL = "https://kaai-api.akadev.me";
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 );
